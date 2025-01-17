c00e201cda680978a3eb6154535ea16d3a575755
Mayokun <mayokunlaoshelawal@gmail.com>
2025-01-17 18:01:58 +0100

    new

--- BasicDirectXtutorial/Window.h
+++ BasicDirectXtutorial/Window.h
@@ -1,9 +1,28 @@
 #pragma once
 #include <Windows.h>
 #include<optional>
+#include"ChiliWin.h"
+
 using namespace std;
+
 class Window {
-public:
+private:
+	//singleton manages registration/cleanup of window class
+	class WindowClass {
+	public:
+		static const char* GetName() noexcept;
+		static HINSTANCE GetInstance() noexcept;
+	private:
+		WindowClass() noexcept;
+		~WindowClass();
+		WindowClass(const WindowClass&) = delete;
+		WindowClass& operator=(const WindowClass&) = delete;
+		static constexpr const char* wndClassName = "Chili Direct3D Engine Window";
+		static WindowClass wndClass;
+		HINSTANCE hInstance;
+	};
+
+public:  
 	Window(int width, int height, const char* name);
 	~Window();
 	static optional<int> ProcessMessages();
